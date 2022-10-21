6b254ef9f5b6fbe18181d93bbc77b95bd8114302
Robert Schwindler <robertmichaelschwindler@gmail.com>
2022-10-21 07:22:06 -0400

    FINAL1.2

--- scripts/javascript.js
+++ scripts/javascript.js
@@ -10,11 +10,11 @@ function handleTabletChange(e) {
     if (e.matches) {
       // Then log the following message to the console
         console.log('Media Query Matched!')
-        heroPic.src = '../images/hero-mobile.jpg';
+        heroPic.src = '/images/hero-mobile.jpg';
     }
     else{
         console.log('Big Screen')
-        heroPic.src = '../images/hero-desktop.jpg';
+        heroPic.src = '/images/hero-desktop.jpg';
     }
   }
 
@@ -41,10 +41,6 @@ emailBtn.addEventListener('click', function(){
   console.log(passEmail);
   validateEmail(passEmail);
 })
-
-
-
-
 
   // Register event listener
   mediaQuery.addEventListener('change', handleTabletChange);
