43f5514f150dfebbe8a8f733ab63e178fc47aa64
Robert Schwindler <robertmichaelschwindler@gmail.com>
2022-10-21 07:08:08 -0400

    FINAL

--- scripts/javascript.js
+++ scripts/javascript.js
@@ -1,10 +1,9 @@
 'use strict'
 const mediaQuery = window.matchMedia('(max-width: 1000px)');
 const heroPic = document.getElementById('hero');
+const email = document.getElementById('email');
+const emailError = document.getElementById('emailError');
 
-
-// Create a media condition that targets viewports at least 1000px wide
-;
 
 function handleTabletChange(e) {
     // Check if the media query is true
@@ -12,16 +11,42 @@ function handleTabletChange(e) {
       // Then log the following message to the console
         console.log('Media Query Matched!')
         heroPic.src = '../images/hero-mobile.jpg';
-
     }
     else{
         console.log('Big Screen')
         heroPic.src = '../images/hero-desktop.jpg';
     }
   }
-  
+
+function validateEmail(inputText)
+{
+    let mailFormat = /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+    if(inputText.match(mailFormat)){
+        emailError.innerHTML = 'Thank you!';
+        email.value = '';
+        return true;
+        
+	  }else {
+        emailError.innerHTML = 'Looks like this is not an email';
+        email.value = '';
+        email.innerHTML = '' 
+        email.focus();
+        return false;
+       
+      }
+}
+
+emailBtn.addEventListener('click', function(){
+  let passEmail = email.value;
+  console.log(passEmail);
+  validateEmail(passEmail);
+})
+
+
+
+
+
   // Register event listener
   mediaQuery.addEventListener('change', handleTabletChange);
   // Initial check
   handleTabletChange(mediaQuery);
-
